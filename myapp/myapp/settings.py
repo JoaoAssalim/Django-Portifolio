655b3701afda3112e82f179f84a583741142f5d1
Joao Assalim <joassalim@gmail.com>
2023-04-15 20:20:54 -0300

    settings e requirements

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-tu_@o(j03uq9g6by40vlqn_*t!f3=s3w)$6yr+a0@(g&za8rip
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
